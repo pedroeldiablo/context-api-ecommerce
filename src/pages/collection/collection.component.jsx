7caa07a2d6f4334e609627a6937a3f7409840564
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-09 14:32:17 +0100

    updating components to use context api and fixing typo

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,7 +8,7 @@ import './collection.styles.scss';
 
 const CollectionPage = ({ match }) => {
   const collections = useContext(CollectionsContext);
-  const collection = collections(match.params.collectionId);
+  const collection = collections[match.params.collectionId];
   const { title, items } = collection;
   return (
     <div className='collection-page'>
