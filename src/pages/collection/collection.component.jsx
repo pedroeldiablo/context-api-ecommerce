bc94715afc6ad893884e405182473c0dd38ba62b
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-09 12:16:11 +0100

    replace redux with contextAPI in collection component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,34 +1,26 @@
-import React from 'react';
+import React, { useContext }from 'react';
 
-import CollectionContext from '../../contexts/collections/collections.context';
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import CollectionItem from '../../components/collection-item';
-
+import CollectionsContext from '../../contexts/collections/collections.context';
 
 import './collection.styles.scss';
 
 const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections(match.params.collectionId);
+  const { title, items } = collection;
   return (
-    <CollectionContext.Consumer>
-      {
-        collections => {
-          const collection = collections[match.params.colleectionId];
-          const { title, items } = collection;
-          return (
-            <div className='collection-page'>
-              <h2 className='title'>{title}</h2>
-              <div className='items'>
-                {items.map(item => (
-                  <CollectionItem key={item.id} item={item} />
-                ))}
-              </div>
-            </div>
-          )
-          
-        }
-      }
-    </CollectionContext.Consumer>
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
   );
 };
+
 
 export default CollectionPage;
