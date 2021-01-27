447b44029dcaff55d91562cbb32201463f2833ca
Peter Williams <pedroeldiablo@yahoo.com>
2021-01-27 01:50:50 +0000

    setting up and building basic tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  ReactDOM.render((
+   <BrowserRouter>
+    <App />
+   </BrowserRouter>), div);
+ });
+
+describe('Test suits for MyComponentWithLink', () => {
+  it('should match with snapshot', () => {
+  const tree = renderer
+    .create(
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    )
+    .toJSON();
+   expect(tree).toMatchSnapshot();
+   });
+  });
