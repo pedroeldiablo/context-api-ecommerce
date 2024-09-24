e65e74ed94dae0396b55512a24f755fbf263f4cb
Peter Williams <pedroeldiablo@yahoo.com>
2024-09-24 15:45:51 +0100

    split bundle and lazy load with suspense

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,8 +5,10 @@ import { CollectionsOverview } from '../../components/collections-overview/colle
 import { CollectionPage } from '../collection/collection.component';
 
 export const ShopPage = ({ match }) => (
-  <div className='shop-page'>
+  <div className="shop-page">
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
+
+export default ShopPage;
